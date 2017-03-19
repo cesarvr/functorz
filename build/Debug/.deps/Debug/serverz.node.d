31e373485999ca9b282b7864cb07a9403ea2d3cd
cesar <cesarv01@yahoo.com>
2017-03-19 15:27:11 +0000

    removing problem in fibunacci.js example

--- build/Debug/.deps/Debug/serverz.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Debug/serverz.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Debug  -o Debug/serverz.node Debug/obj.target/serverz/lib/serverz.o 
--- scripts/fibunacci.js
+++ scripts/fibunacci.js
@@ -1,4 +1,4 @@
-console.log('require->', require)
+//console.log('require->', require)
 function fib(n){
     if(n < 0) return 1; 
       return fib(n-1) + fib(n-2);
