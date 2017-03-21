b87e3691bd2bd62b9b7effcf67e3ff2641152f33
cesar_vldz <cesar.valdez@feedhenry.com>
2017-03-21 13:12:31 +0000

    adding some ui and starting the work in server

--- application.js
+++ application.js
@@ -1,24 +1,55 @@
-'use strict'; 
+'use strict';
 
 var express = require('express')
 var functor = require('./build/Debug/serverz');
-
+var _ = require('underscore');
+var bodyParser = require('body-parser')
 var app = express()
 
+const ERROR_MSG_1 = "Missing parameters in the request call, this service accept {code:<string with javascript code>, name:<function name> }";
+const ERROR_MSG_2 = "Error at compile time: ";
+const MSG_COMPILE_STORE = ' has been compiled and is ready to use.';
+
 app.use(express.static('public'))
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
 
-var fmap = {}; 
+var fmap = {};
 
-app.post('/function', function (req, res) {
+app.post('/function', function(req, res) {
 
-  console.log(req.param); 
+    console.log(req.body);
+    if (!_.isUndefined(req.body.code) && !_.isUndefined(req.body.name)) {
+
+        try {
+            fmap[req.body.name] = functor.compile(req.body.code);
+        } catch (e) {
+
+            res.status(406).send({
+                result: ERROR_MSG_2 + e
+            });
+        }
+
+        console.log('compiled result->', fmap[req.body.name]);
+
+        res.send({
+            result: req.body.name + MSG_COMPILE_STORE
+        });
+
+    } else
+        res.status(406).send({
+            result: ERROR_MSG_1
+        });
+
 })
 
-app.get('/function/:id', function (req, res) {
+
+app.get('/function/:id', function(req, res) {
 
 })
 
-app.listen(8080, function () {
+app.listen(8080, function() {
     console.log('Example app listening on port 8080!')
 })
-
--- public/app.js
+++ public/app.js
@@ -1,12 +1,32 @@
-'use strict' 
+'use strict';
 
-(
-  $('#send').click(function(){
+(function() {
+    function createFn() {
+        return {
+            code: $('#code').val(),
+            name: $('#fname').val()
+        };
+    }
 
-    
+
+    $('#send').click(function(e) {
+        e.preventDefault();
+        var $console = $('.result');
 
 
-  });
+        debugger;
+        var compiling = $.post('/function', createFn());
+        
+        compiling.done(function(msg){
+          $console.removeClass('fail').addClass('sucess').html(msg.result);
+        });
+
+        compiling.fail(function(msg){
+          var result = JSON.parse(msg.responseText).result; 
+          $console.removeClass('sucess').addClass('fail').html(result);
+        });
 
 
-)();
+        console.log('boom')
+    });
+})();
